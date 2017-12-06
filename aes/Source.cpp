fb9405dccd5adb17b7bfb2359df5d29ee0a932ca
617475jordan <617475jordan@sina.com>
2017-12-06 19:44:37 +0800

    change

---
+++ aes/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>  
+#include "AES.h"  
+ 
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	AES *m_aes = new AES();
+	string str1 = "qwertyuisfdlsajdxcvnkhsakfh1332487";
+	cout << "加密前:" << str1 << endl;
+	string str2 = m_aes->EncryptionAES(str1);
+	cout << "加密后:" << str2 << endl;
+	string str3 = m_aes->DecryptionAES(str2);
+	cout << "解密后:" << str3 << endl;
+	delete[] m_aes;
+	system("pause");
+	return 0;
+}
